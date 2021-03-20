241fd0018e99f2c9f482f2d7daef9aeba30b37cc
Mikael Araya <mikaelaraya12@gmail.com>
2021-03-21 00:54:11 +0300

    Add product list style

--- storefront/modules/products/components/ProductList.tsx
+++ storefront/modules/products/components/ProductList.tsx
@@ -1,7 +1,5 @@
-import Link from 'next/link';
-
 import useProductListQuery from '../hooks/useProductListQuery';
-import getProductMediaUrl from '../utils/getProductMediaUrl';
+import ProductListItem from './ProductListItem'
 
 const ProductList = () => {
   const { products } = useProductListQuery();
@@ -10,27 +8,10 @@ const ProductList = () => {
     <div className="container">
       <div className="row">
         {products.map((product) => (
-          <div
-            key={product._id}
-            className="col-sm-6 col-lg-4 product-list-item"
-          >
-            <Link href="/product/[slug]" as={`/product/${product.texts.slug}`}>
-              <a className="product-list-item-overlay" />
-            </Link>
-            <div className="product-list-item-inner">
-              <div className="product-list-header">
-                <h3 className="px-2 my-3">{product?.texts?.title}</h3>
-              </div>
-              <img src={getProductMediaUrl(product)} />
-              <div className="p-2">
-                <h4 className="my-0">
-                  CHF {product?.simulatedPrice?.price?.amount / 100}
-                </h4>
-                <h4 className="mb-0">{product?.texts?.subtitle}</h4>
-                <p>{product?.texts?.description?.split('\n')[0]}</p>
-              </div>
-            </div>
+          <div key={product._id} className="col-sm-6 col-lg-3 mb-4 mx-auto">
+          <ProductListItem product={product} />
           </div>
+          
         ))}
       </div>
     </div>
